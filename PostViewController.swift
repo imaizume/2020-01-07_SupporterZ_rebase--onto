482370d5d3c94ba7ab483fafb48f2a0094e3cf47
Tomohiro Imaizumi <info@imaizu.me>
2020-01-05 20:35:24 +0900

    投稿画面のViewControllerを作成

---
+++ PostViewController.swift
@@ -0,0 +1,11 @@
+import UIKit
+
+class PostViewController: UIViewController {
+  override func viewDidLoad() {
+    super.viewDidload()
+  }
+
+  override func viewDidAppear(_ animated: Bool) {
+    super.viewDidAppear(animated)
+  }
+}
