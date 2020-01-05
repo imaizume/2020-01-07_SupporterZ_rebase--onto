499b36614de520c6a31bba8d4eb27aee9fa55411
Tomohiro Imaizumi <info@imaizu.me>
2020-01-05 20:43:48 +0900

    範囲外のインデックス参照によるランタイムエラーを防止するための実装を追加

--- ArrayExtension.swift
+++ ArrayExtension.swift
@@ -2,4 +2,8 @@ import Foundation
 
 // Arrayの便利な拡張機能
 extension Array {
+  /// 範囲外のインデックス参照によるランタイムエラーを防止するための実装
+  subscript(getOrNil index: Index) -> Element? {
+    return indices.contains(index) ? self[index] : nil
+  }
 }
